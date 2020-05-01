0ab99b1402afc09dad4f1c95907dcab605544184
Joel <joel.mong.au@gmail.com>
2020-05-01 15:52:03 +1000

    Add color selection

--- Expensee/Scenes/AddCategory/AddCategoryViewController.swift
+++ Expensee/Scenes/AddCategory/AddCategoryViewController.swift
@@ -16,7 +16,7 @@ class AddCategoryViewController: UIViewController {
     @IBOutlet weak var limitTextField: UITextField!
     @IBOutlet weak var currencySegmentControl: UISegmentedControl! {
         didSet {
-            presenter?.setMonthlyLimitCurrency(currency(ofSegment: currencySegmentControl))
+            presenter?.didSelectMonthlyLimitCurrency(currency(ofSegment: currencySegmentControl))
         }
     }
 
@@ -32,7 +32,7 @@ class AddCategoryViewController: UIViewController {
         limitTextField.delegate = amountTextFieldDelegate
         amountTextFieldDelegate.updateAction = { [weak presenter] _, result in
             result.do(ifLeft: { (amount) in
-                presenter?.setMonthlyLimit(amount?.floatValue)
+                presenter?.didSelectMonthlyLimit(amount?.floatValue)
             }, ifRight: { error in
                 print("\(error)")
             })
@@ -41,7 +41,7 @@ class AddCategoryViewController: UIViewController {
         nameTextField.delegate = anyTextFieldDelegate
         anyTextFieldDelegate.updateAction = { [weak presenter] _, result in
             result.do(ifLeft: { (name) in
-                presenter?.setCategoryName(name)
+                presenter?.didSelectCategoryName(name)
             }, ifRight: { error in
                 print("\(error)")
             })
@@ -60,7 +60,7 @@ class AddCategoryViewController: UIViewController {
         let dataSource = SimpleTableDataSource<ColorCellModel, UITableViewCell>()
 
         dataSource.binder = { (row: ColorCellModel, cell: UITableViewCell) in
-            cell.textLabel?.text = row.color
+            cell.backgroundColor = UIColor(row.color)
             cell.accessoryType = row.isChecked ? .checkmark : .none
         }
 
@@ -79,6 +79,7 @@ class AddCategoryViewController: UIViewController {
         let provider = TableViewProvider<ColorCellModel, UITableViewCell>(tableView: tableView)
         provider.dataSource = dataSource
         provider.dataSource?.tapAction = { selected in
+            self.presenter.didSelectCategoryColor(selected.id)
         }
 
         return provider
@@ -113,13 +114,13 @@ protocol AddCategoryControlling: class {
     
     func viewIsReady()
 
-    func setMonthlyLimit(_ limit: Float?)
+    func didSelectMonthlyLimit(_ limit: Float?)
 
-    func setMonthlyLimitCurrency(_ currency: String?)
+    func didSelectMonthlyLimitCurrency(_ currency: String?)
 
-    func setCategoryName(_ name: String?)
+    func didSelectCategoryName(_ name: String?)
 
-    func setCategoryColor(_ color: String?)
+    func didSelectCategoryColor(_ color: UUID?)
 }
 
 final class AddCategoryPresenter {
@@ -152,34 +153,39 @@ final class AddCategoryPresenter {
 
     struct Category {
         let name: String?
-        let color: String?
+        let color: UUID?
     }
 }
 
 extension AddCategoryPresenter: AddCategoryControlling {
     
-    func viewIsReady() {
+    private func colorsCellModels(withSelection selection: UUID?) -> [ColorCellModel] {
         let categoriesColor = interactor.listColors(request: ListColorsRequest())
-        let cells = categoriesColor.colors.map {
-            ColorCellModel(color: $0.color, isChecked: false, id: $0.uuid)
+        return categoriesColor.colors.map {
+            ColorCellModel(color: $0.color, isChecked: selection != nil ? selection == $0.uuid : false, id: $0.uuid)
         }
-        view?.displayColors(colors: cells)
+    }
+
+    func viewIsReady() {
+        view?.displayColors(colors: colorsCellModels(withSelection: nil))
         view?.setSaveButtonEnable(false)
     }
     
-    func setMonthlyLimit(_ limit: Float?) {
+    func didSelectMonthlyLimit(_ limit: Float?) {
         self.monthlyLimit = MonthlyLimit(limit: limit, currency: monthlyLimit?.currency)
     }
     
-    func setMonthlyLimitCurrency(_ currency: String?) {
+    func didSelectMonthlyLimitCurrency(_ currency: String?) {
         self.monthlyLimit = MonthlyLimit(limit: monthlyLimit?.limit, currency: currency)
     }
 
-    func setCategoryName(_ name: String?) {
+    func didSelectCategoryName(_ name: String?) {
         category = Category(name: name, color: category?.color)
     }
 
-    func setCategoryColor(_ color: String?) {
+    func didSelectCategoryColor(_ color: UUID?) {
         category = Category(name: category?.name, color: color)
+        let cellModel = colorsCellModels(withSelection: color)
+        view?.displayColors(colors: cellModel)
     }
 }
--- Expensee/Scenes/AddCategory/CategoryColorUseCase.swift
+++ Expensee/Scenes/AddCategory/CategoryColorUseCase.swift
@@ -15,16 +15,17 @@ protocol CategoryColorUseCaseProtocol {
 
 final class CategoryColorUseCase: CategoryColorUseCaseProtocol {
 
+    private let colors = [
+        ListCategoryUseCaseResponse.CategoryColor(color: "#ff71ce", uuid: UUID()),
+        ListCategoryUseCaseResponse.CategoryColor(color: "#01cdfe", uuid: UUID()),
+        ListCategoryUseCaseResponse.CategoryColor(color: "#05ffa1", uuid: UUID()),
+        ListCategoryUseCaseResponse.CategoryColor(color: "#b967ff", uuid: UUID()),
+        ListCategoryUseCaseResponse.CategoryColor(color: "#fffb96", uuid: UUID()),
+        ListCategoryUseCaseResponse.CategoryColor(color: "#fffeb3", uuid: UUID()),
+        ListCategoryUseCaseResponse.CategoryColor(color: "#666547", uuid: UUID()),
+    ]
+
     func listCategoryColors(request: ListCategoryUseCaseRequest) -> ListCategoryUseCaseResponse {
-        let colors = [
-            ListCategoryUseCaseResponse.CategoryColor(color: "#ff71ce", uuid: UUID()),
-            ListCategoryUseCaseResponse.CategoryColor(color: "#01cdfe", uuid: UUID()),
-            ListCategoryUseCaseResponse.CategoryColor(color: "#05ffa1", uuid: UUID()),
-            ListCategoryUseCaseResponse.CategoryColor(color: "#b967ff", uuid: UUID()),
-            ListCategoryUseCaseResponse.CategoryColor(color: "#fffb96", uuid: UUID()),
-            ListCategoryUseCaseResponse.CategoryColor(color: "#fffeb3", uuid: UUID()),
-            ListCategoryUseCaseResponse.CategoryColor(color: "#666547", uuid: UUID()),
-        ]
         return ListCategoryUseCaseResponse(colors: colors)
     }
 }
