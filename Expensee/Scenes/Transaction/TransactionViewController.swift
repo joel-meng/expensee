3f89eb8c1683f1e53b9819119f9723e98dbdb548
Joel <joel.mong.au@gmail.com>
2020-05-03 08:38:24 +1000

    Separate Presenter

--- Expensee/Scenes/Transaction/TransactionViewController.swift
+++ Expensee/Scenes/Transaction/TransactionViewController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+protocol TransactionPresenting: class {
+
+    func showState(amount: Double?, currency: String?, date: Date?, categoryName: String?, categoryColor: String?)
+
+    func handleSaveReady(_ enabled: Bool)
+}
+
 class TransactionViewController: UIViewController {
 
     private let MAX_TRANSACTION_AMOUNT: Double = 999_999_999_999_999
@@ -161,165 +168,5 @@ extension TransactionViewController: TransactionPresenting {
 
     func handleSaveReady(_ enabled: Bool) {
         saveButton.isEnabled = enabled
-    }
-}
-
-protocol TransactionPresenting: class {
-    
-    func showState(amount: Double?, currency: String?, date: Date?, categoryName: String?, categoryColor: String?)
-
-    func handleSaveReady(_ enabled: Bool)
-}
-
-protocol TransactionControlling: class {
-
-    func viewIsReady()
-
-    func didInputTransactionAmount(_ amount: Double?)
-
-    func didUpdateTransactionCurrency(_ currency: String?)
-
-    func didSelectDate(_ date: Date)
-
-    func didTapCategory()
-
-    func didTapSave()
-}
-
-final class TransactionPresenter {
-
-    private weak var view: TransactionPresenting?
-
-    private var router: TransactionRouting
-
-    private var flavor: SceneFlavor = .save
-
-    private var transaction: Transaction {
-        didSet { didUpdateTransaction(transaction) }
-    }
-
-    private var category: Category {
-        didSet { didUpdateCategory(category) }
-    }
-
-    init(view: TransactionPresenting, router: TransactionRouting, transaction: Transaction?, category: Category?) {
-        self.transaction = transaction ?? Transaction(amount: nil, date: Date(), currency: "NZD")
-        self.category = category ?? Category(id: nil, name: nil, color: nil, limit: .init(amount: nil, currency: nil))
-        self.view = view
-        self.router = router
-    }
-
-    // MARK: - Update
-
-    private func didUpdateTransaction(_ transaction: Transaction?) {
-        print(transaction)
-    }
-
-    private func didUpdateCategory(_ category: Category?) {
-        print(category)
-    }
-
-    private func isDateValidForSaving() -> Bool {
-        return (transaction.amount != nil && category.name != nil && category.color != nil)
-    }
-
-    enum SceneFlavor {
-        case update
-        case save
-    }
-}
-
-extension TransactionPresenter: TransactionControlling {
-
-    // MARK: - State Handling
-    private func setupInitialState(flavor: SceneFlavor) {
-        switch flavor {
-        case .save: displayCurrentState()
-        case .update:
-            break
-        }
-    }
-
-    private func displayCurrentState() {
-        view?.showState(amount: transaction.amount,
-                        currency: transaction.currency,
-                        date: transaction.date ?? Date(),
-                        categoryName: category.name ?? "Select Category",
-                        categoryColor: category.color ?? "#EEEEFF")
-    }
-
-    // MARK: - TransactionControlling
-
-    func viewIsReady() {
-        setupInitialState(flavor: flavor)
-    }
-
-    func didInputTransactionAmount(_ amount: Double?) {
-        transaction = Transaction(amount: amount, date: transaction.date, currency: transaction.currency)
-        view?.handleSaveReady(isDateValidForSaving())
-    }
-
-    func didUpdateTransactionCurrency(_ currency: String?) {
-        transaction = Transaction(amount: transaction.amount, date: transaction.date, currency: currency ?? transaction.currency)
-        view?.handleSaveReady(isDateValidForSaving())
-    }
-
-    func didSelectDate(_ date: Date) {
-        transaction = Transaction(amount: transaction.amount, date: date, currency: transaction.currency)
-        view?.handleSaveReady(isDateValidForSaving())
-    }
-
-    func didTapCategory() {
-        router.routeToCategoryList { [weak self, category] (passBack) in
-            guard let self = self else { return }
-            self.category = passBack.category.map {
-                TransactionPresenter.Category(id: $0.uid, name: $0.name, color: $0.color, limit: $0.budget.map {
-                    TransactionPresenter.Limit(amount: $0.limit, currency: $0.currency)
-                })
-            } ?? category
-            self.displayCurrentState()
-            self.view?.handleSaveReady(self.isDateValidForSaving())
-        }
-    }
-
-    func didTapSave() {
-        guard let amount = self.transaction.amount else { return }
-        guard let categoryId = category.id else { return }
-        let context = CoreDataStore.shared?.context
-        let categoryDTO = CategoryDTO(name: category.name ?? "", color: category.color ?? "", budget: nil, uid: categoryId)
-        let transactionDTO = TransactionDTO(amount: amount, date: Date(), currency: "AAA", uid: UUID(), category: categoryDTO)
-
-        let result = ExpenseTransaction.insert(category: transactionDTO, into: context!)
-        print(result)
-    }
-}
-
-extension TransactionPresenter {
-
-    struct Transaction {
-
-        let amount: Double?
-
-        let date: Date?
-
-        let currency: String?
-    }
-
-    struct Category {
-
-        let id: UUID?
-
-        let name: String?
-
-        let color: String?
-
-        let limit: Limit?
-    }
-
-    struct Limit {
-
-        let amount: Double?
-
-        let currency: String?
     }
 }
