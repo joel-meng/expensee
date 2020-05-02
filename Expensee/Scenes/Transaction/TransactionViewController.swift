97ff0a05ded573dd7e71511db2aa6d545fe0c355
Joel <joel.mong.au@gmail.com>
2020-05-02 12:47:24 +1000

    Add tx details vc

---
+++ Expensee/Scenes/Transaction/TransactionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionViewController.swift
+//  Expensee
+//
+//  Created by Jun Meng on 2/5/20.
+//  Copyright © 2020 Jun Meng. All rights reserved.
+//
+
+import UIKit
+
+class TransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
